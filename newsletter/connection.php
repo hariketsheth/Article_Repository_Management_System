34a36ce23be5ce51ce9449e166d7e97fc4b45e0a
hariketsheth <hariket.sukeshkumar2020@gmail.com>
2021-11-23 19:11:36 +0530

    Added Database Connection and Posts

---
+++ newsletter/connection.php
@@ -0,0 +1,16 @@
+<?php 
+session_start();
+$con = mysqli_connect('DB_HOST', 'DB_USER', 'DB_PASS', 'DB_NAME');
+if (!$con){
+    $_SESSION['connectiondb']="false";
+}
+else {
+     $_SESSION['connectiondb']="true";
+}
+define('CONTACTFORM_RECAPTCHA_SITE_KEY', 'PLACE_YOUR_OWN');
+define('CONTACTFORM_RECAPTCHA_SECRET_KEY', 'PLACE_YOUR_OWN');
+
+?>
+
+
+
